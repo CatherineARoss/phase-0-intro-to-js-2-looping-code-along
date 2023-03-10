5259b6907a0f0382c388298b161f47f7d35fa2a1
Catherine Ross <fibervibes@gmail.com>
2023-03-10 07:03:41 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, value) {
+    let messageNames = []
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown() {
+let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
